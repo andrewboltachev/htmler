b8f78c8a2d1512e6ea20a51daff78eac86b0a0f7
Andrew Boltachev <andrewboltachev@gmail.com>
2014-09-20 22:37:27 +0400

    cleanup

--- file0001.py
+++
@@ -1,8 +0,0 @@
-EMPTY_TAGS = 'br img'
-EMPTY_TAGS = EMPTY_TAGS.split(' ')
-
-def tag(name, *children, **attributes):
-    attributes_str = ' ' + ' '.join([k.replace('_', '-') + '=' + v for k, v in attributes.iteritems()])
-    if name in EMPTY_TAGS:
-        return '<%s%s>%s</%s>'
-    return '<%s%s>%s</%s>'
