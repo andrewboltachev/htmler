932f84777228c3add82fddca2b5d130f0644c982
Andrew Boltachev <andrewboltachev@gmail.com>
2014-11-07 16:37:34 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funniest',
+      version='0.1',
+      description='Write HTML in simple way using pure Python',
+      url='http://github.com/andrewboltachev/htmler',
+      author='Andrew Boltachev',
+      author_email='andrewboltachev@gmail.com',
+      license='MIT',
+      packages=['htmler'],
+      zip_safe=False)
